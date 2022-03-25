7b77cee9882bab00e91d1fa91618cdbf2f8c10da
Donething <donething.net@gmail.com>
2022-03-25 14:23:32 +0800

    改为 大写为操作，小写为参数

--- main.go
+++ main.go
@@ -10,16 +10,16 @@ import (
 	"tools-go/textcoding"
 )
 
+// 大写为操作，小写为参数
 var (
-	// 操作
-	d bool // 下载 JSON 格式的图集
-	h bool // 帮助
-	s bool // 下载字幕
-	t bool // 转换指定目录下指定格式的文件编码
+	D bool // 下载 JSON 格式的图集
+	H bool // 帮助
+	S bool // 下载字幕
+	T bool // 转换指定目录下指定格式的文件编码
 
 	// 参数
 	a string // 地址/路径
-	D string // 目标路径
+	d string // 目标路径
 	f string // 文件的格式
 	k string // 关键字
 )
@@ -36,18 +36,19 @@ var (
 
 func init() {
 	// 操作
-	flag.BoolVar(&d, "d", false, "下载 JSON 图集，如'-d -D /save/dir -a /json/path'")
-	flag.BoolVar(&h, "h", false, "帮助")
-	flag.BoolVar(&t, "t", false, "转换指定目录下指定格式的文件编码为 UTF-8，"+
-		"如'-t -f .txt -a /to/the/path'")
-	flag.BoolVar(&s, "s", false, "下载电影的字幕字幕，"+
-		"如'-s -k 电影名 [-a /path/the/film]'。如果指定电影路径，则自动重命名字幕文件并保存到相同路径")
+	flag.BoolVar(&H, "H", false, "帮助")
+	flag.BoolVar(&D, "D", false, "下载 JSON 图集，"+
+		"如'-D -d /save/dir -a /json/path'")
+	flag.BoolVar(&T, "T", false, "转换指定目录下指定格式的文件编码为 UTF-8，"+
+		"如'-T -f txt -a /to/the/path'")
+	flag.BoolVar(&S, "S", false, "下载电影的字幕字幕，"+
+		"如'-S -k 电影名 [-a /path/the/film]'。如果指定电影路径，则自动重命名字幕文件并保存到相同路径")
 
 	// 参数
-	flag.StringVar(&a, "a", "", "地址/路径")
-	flag.StringVar(&D, "D", "", "目标路径")
-	flag.StringVar(&f, "f", "",
-		"文件格式，可以用逗号分隔多个格式，如'txt,cue,srt'。可通过数字快速指定文件类型，1：字幕格式")
+	flag.StringVar(&a, "a", "", "源地址/路径")
+	flag.StringVar(&d, "d", "", "目标路径")
+	flag.StringVar(&f, "f", "", "文件格式，可以用逗号分隔多个格式。"+
+		"如'txt,cue,srt'。还可通过数字快速指定某类文件，1：字幕文件格式")
 	flag.StringVar(&k, "k", "", "关键字")
 
 	flag.Usage = usage
@@ -63,15 +64,15 @@ func main() {
 	}
 
 	// 根据参数指定操作
-	if h {
+	if H {
 		flag.PrintDefaults()
-	} else if d {
-		color.Info.Tips("开始下载 JSON 格式的图集'%s'，保存到'%s'\n", a, D)
-		downloads.DLImgs(D, a)
-	} else if t {
+	} else if D {
+		color.Info.Tips("开始下载 JSON 格式的图集'%s'，保存到'%s'\n", a, d)
+		downloads.DLImgs(d, a)
+	} else if T {
 		color.Info.Tips("开始执行转换文本编码。格式：'%s'，路径：'%s'\n", format, a)
 		textcoding.TransformDir(a, format)
-	} else if s {
+	} else if S {
 		color.Info.Tips("开始尝试下载'%s'的字幕\n", k)
 		film.DLSubtitle(k, a)
 	} else {
@@ -80,7 +81,7 @@ func main() {
 }
 
 func usage() {
-	_, err := fmt.Fprintf(os.Stderr, `Usage: tools-go [-t] [-f format] [-a addr/path]
+	_, err := fmt.Fprintf(os.Stderr, `Usage: tools-go [-T] [-f format] [-a addr/path]
 Options:
 `)
 	flag.PrintDefaults()
